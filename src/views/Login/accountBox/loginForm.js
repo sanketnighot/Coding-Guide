14c25a3e3010b97d379523f5a2cc1807acc80b22
Rohit <infiniterohitsurya@gmail.com>
2021-04-26 19:06:34 +0530

    login signup

--- src/views/Login/accountBox/loginForm.js
+++ src/views/Login/accountBox/loginForm.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import {useState} from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -12,17 +13,37 @@ import { AccountContext } from "./accountContext";
 
 export function LoginForm(props) {
   const { switchToSignup } = useContext(AccountContext);
+  var [email, setEmail] = useState("");
+  var [password, setPassword] = useState("");
 
+  const onChangeHandler = (event) =>{
+    const { name, value } = event.currentTarget;
+    if (name === "email"){
+      setEmail(value);
+    }
+    else if (name === "password"){
+      setPassword(value);
+    }
+  }
+  const fetchUser = async ()=>{
+    await fetch(`http://localhost:8000/user-email=${email}&password=${password}`).then((response) => response.json()).then((data)=>{
+      if (data.message) {
+      alert("Login Failed");
+    } else {
+      alert(`Name: ${data.name}`);
+    }
+    });
+  }
   return (
     <BoxContainer>
       <FormContainer>
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
+      <Input name="email" value={email} onChange={(event) => { onChangeHandler(event) }} type="email" placeholder="Email" />
+      <Input name="password" value={password} onChange={(event) => { onChangeHandler(event) }} type="password" placeholder="Password" />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
       <MutedLink href="#">Forget your password?</MutedLink>
       <Marginer direction="vertical" margin="1.6em" />
-      <SubmitButton type="submit">Signin</SubmitButton>
+      <SubmitButton type="submit" onClick={()=>{fetchUser()}}>Signin</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Don't have an accoun?{" "}
--- src/views/Login/accountBox/signupForm.js
+++ src/views/Login/accountBox/signupForm.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import  {useState} from 'react'
 import {
   BoldLink,
   BoxContainer,
@@ -9,20 +10,69 @@ import {
 } from "./common";
 import { Marginer } from "../marginer";
 import { AccountContext } from "./accountContext";
+const axios = require('axios');
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
+
+
+
+
 
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
+  var [name, setName] = useState("");
+  var [email, setEmail] = useState("");
+  var [password, setPassword] = useState("");
+  var [cpassword, setCPassword] = useState("");
+
+  const onChangeHandler = (event) =>{
+    const { name, value } = event.currentTarget;
+    if (name === "text"){
+      setName(value);
+      console.log(value);
+    }
+    else if (name === "email"){
+      setEmail(value);
+    }
+    else if (name === "password"){
+      setPassword(value);
+    }
+    else if (name === "cpassword"){
+      setCPassword(value);
+    }
+  }
+  const createUser = async ()=>{
+    if (password === cpassword) {
+    const data = {
+      name: name,
+      email: email,
+      password: password
+    }
+    console.log(data);
+    await axios.post('http://localhost:8000/signup', data).then((response) => {
+      alert("Signup Successful");
+      setName("");
+      setEmail("");
+      setPassword("");
+      setCPassword("");
+    }).catch( (error)=>{console.log(error); });
+  } else {
+    alert("Password Don't Match");
+    setPassword("");
+    setCPassword("");
+  }
+  }
 
   return (
     <BoxContainer>
       <FormContainer>
-        <Input type="text" placeholder="Full Name" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
-        <Input type="password" placeholder="Confirm Password" />
+        <Input name="text" value={name} onChange={(event) => { onChangeHandler(event) }} type="text" placeholder="Full Name" />
+        <Input name="email" value={email} onChange={(event) => { onChangeHandler(event) }} type="email" placeholder="Email" />
+        <Input name="password" value={password} onChange={(event) => { onChangeHandler(event) }} type="password" placeholder="Password" />
+        <Input name="cpassword" value={cpassword} onChange={(event) => { onChangeHandler(event) }} type="password" placeholder="Confirm Password" />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
-      <SubmitButton type="submit">Signup</SubmitButton>
+      <SubmitButton type="submit" onClick={()=>{createUser()}}>Signup</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Already have an account?
