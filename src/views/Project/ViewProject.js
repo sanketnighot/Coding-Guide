46f826596a7dedae8bc8c302d9eef1b2e927de3e
Rohit <infiniterohitsurya@gmail.com>
2021-05-08 13:22:56 +0530

    bfwe

--- src/views/Project/ViewProject.js
+++ src/views/Project/ViewProject.js
@@ -37,7 +37,6 @@ export default function ViewProject(props) {
                     </GridItem>
                     <GridItem>
                         <GridContainer >
-                            
                         </GridContainer>
                     </GridItem>
                 </GridItem>
